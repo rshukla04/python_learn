0a4b46fcaf06fda3f5f2c73634ae12670937e4b8
rshukla04 <65308005+rshukla04@users.noreply.github.com>
2024-06-07 22:51:37 -0500

    Add files via upload

---
+++ checkDriverAge.py
@@ -0,0 +1,13 @@
+def checkDriverAge(age):
+    age = 0
+    age = input("What is your age?: ")
+
+    if int(age) < 18:
+        print("Sorry, you are too young to drive this car. Powering off")
+    elif int(age) > 18:
+        print("Powering On. Enjoy the ride!");
+    elif int(age) == 18:
+        print("Congratulations on your first year of driving. Enjoy the ride!")
+
+
+print(checkDriverAge(92))
